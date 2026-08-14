731051fb65f40c3dd8c00456dbd8ad6d31c9efde
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-14 12:13:40 +0100

    Use exact users UID app path

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v8';
+const CACHE = 'retireme-v9';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
