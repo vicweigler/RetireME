569124619557a589aa7156b2f9263e06c55bd102
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-14 13:08:18 +0100

    Release v3.1.23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v9';
+const CACHE = 'retireme-v10';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
