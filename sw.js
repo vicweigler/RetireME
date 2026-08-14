e345f66f6c72975c4ae9a62765b8381b0413fbaa
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-14 14:41:28 +0100

    Tighten income model layout and modal scrolling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v12';
+const CACHE = 'retireme-v13';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
