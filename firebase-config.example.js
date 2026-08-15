9c11935ed6787697259e2f4aa890e2a024b9cb21
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-15 09:49:54 +0100

    Secure Firebase config and release V3.2

---
+++ firebase-config.example.js
@@ -0,0 +1,8 @@
+window.RETIREME_FIREBASE_CONFIG = {
+  apiKey: 'replace-with-a-rotated-and-restricted-browser-key',
+  authDomain: 'retireme-prod.firebaseapp.com',
+  projectId: 'retireme-prod',
+  storageBucket: 'retireme-prod.firebasestorage.app',
+  messagingSenderId: '448172346101',
+  appId: '1:448172346101:web:3d4f19118df9ae1aba13bc'
+};
