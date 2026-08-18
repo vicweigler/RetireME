d61c4c5adc6d2147e7576916ccbc878dc9b52b15
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 00:49:15 +0100

    V3.5: Fix iOS tab bar safe area with explicit spacer element

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v15';
+const CACHE = 'retireme-v16';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
