227532e60520bff55dfa0f7a093f3059ad001e22
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 20:57:16 +0100

    V4.0: Match Invest-In-It tab bar pattern - fixed 56px row, centred icons, full safe-area

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v21';
+const CACHE = 'retireme-v22';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
