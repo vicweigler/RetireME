bd75ce22e1a804a1aa1321911e18609b1175b4a9
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 21:15:58 +0100

    V4.5: Tab bar 10px taller, position unchanged

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v26';
+const CACHE = 'retireme-v27';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
