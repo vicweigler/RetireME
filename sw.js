ba9414441106bfb166cd36a7e07a4f3b053c5eb8
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 21:21:07 +0100

    Revert "V4.7: Tabbar 58px visible height, bottom -20px"
    This reverts commit a8a33a86f0918323fb67bba9ab054aa0b37f64af.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v29';
+const CACHE = 'retireme-v28';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
