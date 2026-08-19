9dcc2c7951478f845498cb5f389bb9feb59aeb9b
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 21:10:27 +0100

    V4.3: Reduce tab bar height by ~40px

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v24';
+const CACHE = 'retireme-v25';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
