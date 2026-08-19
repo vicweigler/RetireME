f1f7e80a373ab313913f9937071d5c08537977d4
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 21:26:08 +0100

    V4.9: Tabbar bottom -5px

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v30';
+const CACHE = 'retireme-v31';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
