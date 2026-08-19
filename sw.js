e28c17faf632d63db2911e76ee786c1ffc77b14d
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 21:07:05 +0100

    V4.2: Remove safe-area cap, increase button padding to match Invest-In-It height

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v23';
+const CACHE = 'retireme-v24';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
