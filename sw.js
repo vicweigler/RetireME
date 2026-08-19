b620d803dbdb00f0bbab0dc76a5e4c5def4f7ca3
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-08-19 22:37:23 +0100

    fix: tabbar-react tweak, bump v6.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v44';
+const CACHE = 'retireme-v45';
 const ASSETS = ['./', './index.html', './icon.png', './apple-touch-icon.png', './manifest.json'];
 
 self.addEventListener('install', e => {
