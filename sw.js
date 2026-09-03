3721fd580ee81fa72bd922b019bb6cc1d5266276
vicweigler <151912694+vicweigler@users.noreply.github.com>
2026-09-03 19:09:16 +0100

    Bump app version to v12.12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'retireme-v72';
+const CACHE = 'retireme-v73';
 // Exclude HTML from pre-cache so the navigate handler always serves it fresh.
 const ASSETS = ['./icon.png', './apple-touch-icon.png', './manifest.json'];
 
